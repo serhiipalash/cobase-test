f2e7821b768c27deb87af734399708a61b3db71c
serhiipalash <serhii.palash@gmail.com>
2017-01-23 15:49:01 +0200

    add editTask round button, add styles for mobile view

--- src/js/components/organisms/TaskBoard/index.js
+++ src/js/components/organisms/TaskBoard/index.js
@@ -6,6 +6,8 @@ import TaskBoardGallery from '../../molecules/TaskBoardGallery';
 import TaskBoardChecklist from '../../molecules/TaskBoardChecklist';
 import UserPicture from '../../atoms/UserPicture';
 
+import editRound from '../../../../../static/img/edit_round.png';
+
 import './style.scss';
 
 
@@ -13,6 +15,10 @@ class TaskBoard extends Component {
   /* eslint-disable */
   employeePictureClicked(id) {
     console.info('Employee picture clicked', id);
+  }
+
+  editRoundButtonClicked() {
+    console.info('Edit Round Button clicked', id);
   }
   /* eslint-enable */
 
@@ -25,7 +31,14 @@ class TaskBoard extends Component {
     if (task) {
       return (
         <div className="task_board">
-          <h2 className="task_board__header">{task.name}</h2>
+          <h2 className="task_board__header">
+            {task.name}
+            <img
+              className="task_board__edit_round_button"
+              src={editRound}
+              onClick={this.editRoundButtonClicked.bind(this)}
+            />
+          </h2>
           <div className="task_board__description">{task.description}</div>
           <div className="task_board__employees">
             {task.employees ? task.employees.map((id, index) =>
@@ -36,7 +49,7 @@ class TaskBoard extends Component {
                   onClick={this.employeePictureClicked.bind(this, id)}
                 /> : null
             ) : null}
-            {task.employees.length > 3 ?
+            {task.employees && task.employees.length > 3 ?
               <i className="task_board__employees_plus_button">+{task.employees.length - 3}</i> : null}
           </div>
           <TaskBoardTags tags={task.tags} />
